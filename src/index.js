5f97eebb2edd621ed1cb9022e3b64eccbf0b4678
Steve Kinney <hello@stevekinney.net>
2019-11-21 15:39:30 -0600

    Implement thunk

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import isFunction from 'lodash/isFunction';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -9,8 +10,79 @@ import dummyData from './dummy-data';
 
 import './styles.scss';
 
+const reducer = (state, action) => {
+  if (action.type === 'LOADING') {
+    return {
+      characters: [],
+      loading: true,
+      error: null,
+    };
+  }
+
+  if (action.type === 'RESPONSE_COMPLETE') {
+    return {
+      characters: action.payload.characters,
+      loading: false,
+      error: null,
+    };
+  }
+
+  if (action.type === 'ERROR') {
+    return {
+      characters: [],
+      loading: false,
+      error: action.payload.error,
+    };
+  }
+
+  return state;
+};
+
+const fetchCharacters = dispatch => {
+  dispatch({ type: 'LOADING' });
+  fetch(endpoint + '/characters')
+    .then(response => response.json())
+    .then(response =>
+      dispatch({
+        type: 'RESPONSE_COMPLETE',
+        payload: { characters: response.characters },
+      }),
+    )
+    .catch(error => dispatch({ type: 'ERROR', payload: { error } }));
+};
+
+const initialState = {
+  error: null,
+  loading: false,
+  characters: [],
+};
+
+const useThunkReducer = (reducer, initialState) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const enhancedDispatch = React.useCallback(
+    action => {
+      console.log(action);
+
+      if (isFunction(action)) {
+        action(dispatch);
+      } else {
+        dispatch(action);
+      }
+    },
+    [dispatch],
+  );
+
+  return [state, enhancedDispatch];
+};
+
 const Application = () => {
-  const [characters, setCharacters] = useState(dummyData);
+  const [state, dispatch] = useThunkReducer(reducer, initialState);
+  const { characters } = state;
+
+  useEffect(() => {
+    dispatch(dispatch => {});
+  }, [dispatch]);
 
   return (
     <div className="Application">
@@ -19,6 +91,9 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
+          <button onClick={() => dispatch(fetchCharacters)}>
+            Fetch Characters
+          </button>
           <CharacterList characters={characters} />
         </section>
       </main>
