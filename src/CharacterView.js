142c00a5c7f193e258f52430d73c0ab7495d0c1d
Claudio <claudifero@gmail.com>
2021-02-01 11:04:06 +0200

    Update

--- src/CharacterView.js
+++ src/CharacterView.js
@@ -25,16 +25,16 @@ const CharacterView = ({ match }) => {
           <strong>Gender</strong>: { character.id ? character.gender : dummyCharacters[match.params.id].gender }
         </li>
         <li>
-          <strong>Hair Color</strong>: { character.id ? character.hair_color : dummyCharacters[match.params.id].hairColor}
+          <strong>Hair Color</strong>: { character.id ? character.hair_color : dummyCharacters[match.params.id].hairColor }
         </li>
         <li>
-          <strong>Heigh</strong>: { character.id ? character.height : dummyCharacters[match.params.id].height}
+          <strong>Heigh</strong>: { character.id ? character.height : dummyCharacters[match.params.id].height }
         </li>
         <li>
-          <strong>Mass</strong>: { character.id ? character.mass : dummyCharacters[match.params.id].mass}
+          <strong>Mass</strong>: { character.id ? character.mass : dummyCharacters[match.params.id].mass }
         </li>
         <li>
-          <strong>Skin Color</strong>: { character.id ? character.skin_color : dummyCharacters[match.params.id].skinColor}
+          <strong>Skin Color</strong>: { character.id ? character.skin_color : dummyCharacters[match.params.id].skinColor }
         </li>
       </ul>
     </>
